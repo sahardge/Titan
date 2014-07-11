cf3229f2be7f7f651d36e63cfe7771e2209a507e
sahardge <sahardge1@catamount.wcu.edu>
2014-07-11 14:06:19 -0400

    Update start-titan.sh

--- start-titan.sh
+++ start-titan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CASSANDRA=`/usr/bin/etcdctl --peers 172.17.42.1:4001 get /cassandraSeed/node1`
-ES=`/usr/bin/etcdctl --peers 172.17.42.1:4001 get /elastic/node1`
+ES=`/usr/bin/etcdctl --peers 172.17.42.1:4001 get /situ/elasticsearch/host
 
 
 mv conf/titan.properties conf/titan.properties.orig
@@ -9,5 +9,5 @@ mv conf/titan.properties conf/titan.properties.orig
 cat conf/titan.properties.orig | sed -e "/storage.index.search.hostname/s/127.0.0.1/""$ES""/" > conf/titan.properties
 
 
-./bin/gremlin.sh -e start-titan.groovy
+./bin/gremlin.sh start-titan.groovy
                   
