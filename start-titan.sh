63309c9c2e46d23ca7f21e40b07fe25db0e8114f
sahardge <sahardge1@catamount.wcu.edu>
2014-07-15 10:28:53 -0400

    Update start-titan.sh

--- start-titan.sh
+++ start-titan.sh
@@ -2,6 +2,7 @@
 CASSANDRA=`/usr/bin/etcdctl --peers 172.17.42.1:4001 get /cassandraSeed/node1`
 ES=`/usr/bin/etcdctl --peers 172.17.42.1:4001 get /situ/elasticsearch/host`
 
+confd -verbose -node 'http://172.17.42.1:4001' -confdir /var/mountedvol/confd 
 
 mv conf/titan.properties conf/titan.properties.orig
 cat conf/titan.properties.orig | sed -e "/storage.hostname/s/127.0.0.1/""$CASSANDRA""/" > conf/titan.properties
